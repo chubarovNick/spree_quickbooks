03f26f134b3c1340b835a5708a01ebcb90603df6
Nick Chubarov <nick.chubarov@gmail.com>
2015-04-16 16:31:57 +0400

    Check inventory adjustment

--- lib/spree_quickbooks/grabbers/inventory.rb
+++ lib/spree_quickbooks/grabbers/inventory.rb
@@ -11,10 +11,12 @@ module SpreeQuickbooks
         Spree::Variant.all.find_each do |variant|
           @item = item_service.find_by_sku(variant.sku, 'Name, QtyOnHand')
           stock_adjustment = @item.quantity_on_hand.to_i - variant.total_on_hand
-          stock_location = Spree::StockLocation.first
-          stock_movement = stock_location.stock_movements.build({quantity: stock_adjustment})
-          stock_movement.stock_item = stock_location.set_up_stock_item(variant)
-          stock_movement.save!
+          if stock_adjustment != 0
+            stock_location = Spree::StockLocation.first
+            stock_movement = stock_location.stock_movements.build({quantity: stock_adjustment})
+            stock_movement.stock_item = stock_location.set_up_stock_item(variant)
+            stock_movement.save!
+          end
         end
       end
     end
