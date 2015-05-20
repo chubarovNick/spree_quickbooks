6d8b0e17b4bf38580ce74ef743f963844b9d481f
Nick Chubarov <nick.chubarov@gmail.com>
2015-05-20 19:43:54 +0400

    Fix variant sky

--- lib/spree_quickbooks/sync/variant.rb
+++ lib/spree_quickbooks/sync/variant.rb
@@ -11,7 +11,11 @@ module SpreeQuickbooks
         service = SpreeQuickbooks::Services::Item.new
         sku_was, new_sku = variant_changes
         item = service.find_by_sku(sku_was)
-        service.update(item, {name: new_sku})
+        if item
+          service.find_or_create_by_sku(new_sku)
+        else
+          service.update(item, {name: new_sku})
+        end
       end
 
     end
